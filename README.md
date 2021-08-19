5a8f37dea7cd485b013125fc8f467609c105c11e
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-08-19 15:22:20 +0100

    write hello world on the lcd1602

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ sudo apt-get install build-essential
 sudo apt-get install g++-arm-linux-gnueabihf
 sudo apt-get install gdb-multiarch
 ```
+
+Init the git submodules using - `git submodule update --init`
--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <lcd1602.h>
 
 int main() {
-    printf("Hello world!\n");
+    int rc;
+
+    rc = lcd1602Init(1, 0x27);
+
+    if(rc)
+    {
+        printf("Initialization error\n");
+        return 1;
+    }
+
+    lcd1602WriteString("Hello");
+    lcd1602SetCursor(0, 1);
+    lcd1602WriteString("World");
+    lcd1602Control(1,0,1);
+
+    getchar();
+    lcd1602Shutdown();
 }
